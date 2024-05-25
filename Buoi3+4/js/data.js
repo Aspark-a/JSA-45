2e23a3bbd2d24878bc693d7f8baaac8504edd300
Aspark-a <155804651+Aspark-a@users.noreply.github.com>
2024-05-25 16:00:22 +0700

    Buoi 3

---
+++ Buoi3+4/js/data.js
@@ -0,0 +1,14 @@
+// kieu du lieu JS ----------------------
+// kieu nguyen thuy:
+/**
+ * string: "abc"
+ * number - interger: 123
+ * number - float: 12.3
+ * null
+ * undefined
+ */
+//kieu phuc tap
+/**
+ * object
+ * array
+ */
